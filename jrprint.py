44ff8aad9eff870eefc33547136eab00f8dfbc33
jryego <junkiy62@gmail.com>
2024-05-30 14:57:07 +0300

    this is my try

---
+++ jrprint.py
@@ -0,0 +1,16 @@
+print("I am an Analyst")
+print('In a cyber command')
+print('He said to me "I will look into it"')
+print('He also said "'+" It's raining outside")
+print("He concluded\n   to\n    me\n     saying\n      'Done'")
+# "This marks the end of my intro class today 30.5.2024"
+#For the assignment I fixed the below code
+print("Day-1 -String Manipulation")
+print("String Concatenation is done with the + sign")
+print('e.g ' + "hello" + " world")
+print("New Lines can be created with" + " \\n")
+print("Also, New Lines can be created with" + " \\" + "n" + " as an alternative")
+print('print("This is the soc team")')
+# I am looking into more differences
+print('Hello'),'\n',print('World')
+print('Hello\nWorld')
