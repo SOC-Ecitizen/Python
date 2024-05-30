cfba9b2bbba7de369a8ab715652d8d8ac0776d25
SOC-Ecitizen <ecitizen23@gmail.com>
2024-05-30 15:09:09 +0300

    Recified

---
+++ matui.py
@@ -0,0 +1,8 @@
+print("Day-1 -string manipulation")
+print('string concatenation  is  done  with the "+" sign')
+print('eg '  + " hello" + " world")
+print("New Lines can be created with \\n")
+print("this is the soc team")
+print ('print("this is the soc team")') 
+
+
